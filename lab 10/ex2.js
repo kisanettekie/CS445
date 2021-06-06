8c90c0eb3be94b70c3dd6eb3b85c26e6160c32d6
kisanettekie <ktekie@miu.edu>
2021-06-06 00:10:04 -0500

    committed

--- lab 10/ex2.js
+++ lab 10/ex2.js
@@ -2,21 +2,21 @@
 
 class User {
     constructor(name) {
-    this.name = name;
+        this.name = name;
     }
-   }
-    
-   class Decorator {
+}
+
+class Decorator {
     constructor(user, id, department) {
-    this.name = user.name;
-    this.department = department;
-    this.id = id
+        this.name = user.name;
+        this.department = department;
+        this.id = id
     };
     log() {
-    console.log(`User: ${this.name}, Department: ${this.department}, Id: ${this.id}`)
+        console.log(`User: ${this.name}, Department: ${this.department}, Id: ${this.id}`)
     }
-   }
-    
-   const user = new User("Kisanet");
-   const decorator = new Decorator(user, 2021, "MSD");
-   decorator.log()
+}
+
+const user = new User("Kisanet");
+const decorator = new Decorator(user, 2021, "MSD");
+decorator.log()
